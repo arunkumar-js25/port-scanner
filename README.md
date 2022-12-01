49ae627f0a1f15b4eec969e7e498819bf896cca1
Arun Kumar <53009824+arunkumar-js25@users.noreply.github.com>
2022-12-01 07:53:29 +0000

    Completed logic

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ https://www.freecodecamp.org/learn/information-security/information-security-pro
 
 ### Task
 Create a port scanner using Python.
+
+### Test Cases
+- If the URL passed into the get_open_ports function is invalid, the function should return the string: "Error: Invalid hostname".
+
+- If the IP address passed into the get_open_ports function is invalid, the function should return the string: "Error: Invalid IP address".
--- port_scanner.py
+++ port_scanner.py
@@ -1,8 +1,23 @@
 import socket
+import common_ports
 
-def get_open_ports(target, port_range):
+def get_open_ports(target, port_range, verbose = False):
     open_ports = []
+    verbose_return = "Open ports for "+target+" (targetIP)\nPORT     SERVICE"
+  
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(5)
 
+    for port in port_range:
+      if s.connect_ex((target, port)):
+        open_ports.append(port)
+        if(verbose):
+          port_name = common_ports.ports_and_services.get(port)
+          if(port_name == None):
+            port_name = ""
+          verbose_return = verbose_return + "\n" + str(port).ljust(4) +"     "+port_name
 
-
+    if(verbose):
+      return verbose_return
+      
     return(open_ports)
